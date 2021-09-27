652baf97462633bdf8ea8e47bbf03d8fb9448c59
cavalcant3 <jmatheusoc269@gmail.com>
2021-09-26 23:16:33 -0300

    first commit

---
+++ EstruturaDeDados_E_Alg./src/com/matheus/estruturadados/vetor.java
@@ -0,0 +1,9 @@
+package com.matheus.estruturadados;
+
+public class vetor {
+    private String[] elementos;
+    public void Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+}
+s
---
+++ EstruturaDeDados_E_Alg./src/oo/intellij/Main.java
@@ -0,0 +1,8 @@
+package oo.intellij;
+
+public class Main {
+//esse é o vetor
+    public static void main(String[] args) {
+
+}
+}
